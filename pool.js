2255aef134efbf5e61ccc23653db9bbe57ae1836
kiba <kiba@quartsoft.com>
2026-03-17 09:08:32 +0200

    refactor pool

--- pool.js
+++ pool.js
@@ -1,25 +1,21 @@
 'use strict';
 
-// TODO: Refactor to respect SoC principle and
-// optimize JavaScript for V8
-
 const poolify = (factory, options, size, max) => {
-    const instances = []; // Preallocate array
-    for (let i = 0; i < size; i++) { // Use Array methods instead of loop
-        const instance = factory(...options); // Avoid array destructuring
-        instances.push(instance);
-    }
+    const instances = Array.from({length: size}).map(() => {
+        return factory.apply(null, options);
+    });
 
-    return (instance) => { // Respect SoC and SOLID/SRP
-        if (instance) { // Avoid if-statement
+
+    return {
+        push: (instance) => {
             if (instances.length < max) {
                 instances.push(instance);
             }
+        },
+        pop: () => {
+            return instances.pop() || factory.apply(null, options);
         }
-        instance = instances.pop(); // Do not reassign incoming parameters
-        if (!instance) instance = factory(...options);
-        return instance;
-    };
+    }
 };
 
 // Usage
@@ -27,6 +23,6 @@ const poolify = (factory, options, size, max) => {
 const createBuffer = (size) => new Uint8Array(size);
 const pool = poolify(createBuffer, [4096], 10, 15);
 
-const instance = pool();
-console.log({ instance });
-pool(instance);
+const instance = pool.pop();
+console.log({instance});
+pool.push(instance);
